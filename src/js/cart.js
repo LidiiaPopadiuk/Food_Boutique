0b82377afad5361ede763fff2e52073444306e69
Брацлавський Захар <100858241+Bratslavskij@users.noreply.github.com>
2025-04-17 20:41:28 +0300

    added form

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,6 +2,7 @@ import tmpl from '../templates/cart.hbs';
 
 const cartList = document.querySelector('.cart__list');
 const cartAmount = document.querySelectorAll('.cart-amount');
+const deleteAll = document.querySelector('.cart__deleteall');
 
 let cartData = JSON.parse(localStorage.getItem('cart'));
 
@@ -10,39 +11,47 @@ if (!Array.isArray(cartData)) {
   localStorage.setItem('cart', JSON.stringify(cartData));
 }
 
-const loadCart = async () => {
+const loadCart = () => {
+  cartList.innerHTML = '';
+
   cartAmount.forEach(el => {
     el.textContent = cartData.length;
   });
 
-  for (const el of cartData) {
-    try {
-      const response = await fetch(
-        `https://food-boutique.b.goit.study/api/products/${el.id}`,
-      );
-      const product = await response.json();
+  const fetchProducts = async () => {
+    for (const el of cartData) {
+      try {
+        const response = await fetch(
+          `https://food-boutique.b.goit.study/api/products/${el.id}`,
+        );
+        const product = await response.json();
 
-      cartList.insertAdjacentHTML(
-        'beforeend',
-        tmpl({
-          id: product._id,
-          name: product.name,
-          desc: product.desc,
-          img: product.img,
-          category: product.category,
-          price: '$' + product.price,
-          size: product.size,
-          popularity: product.popularity,
-          amount: el.amount,
-        }),
-      );
-    } catch (error) {
-      console.error('Error loading product:', error);
+        cartList.insertAdjacentHTML(
+          'beforeend',
+          tmpl({
+            id: el.id,
+            name: product.name,
+            desc: product.desc,
+            img: product.img,
+            category: product.category,
+            price: '$' + product.price,
+            size: product.size,
+            popularity: product.popularity,
+            amount: el.amount,
+          }),
+        );
+      } catch (error) {
+        console.error('Error loading product:', error);
+      }
     }
-  }
+  };
+
+  fetchProducts();
 };
 
 export const add2Cart = id => {
+  if (!id) throw new Error('ID should be in add2Cart usage!!!');
+
   const existingItem = cartData.find(item => item.id === id);
 
   if (existingItem) {
@@ -54,23 +63,51 @@ export const add2Cart = id => {
   localStorage.setItem('cart', JSON.stringify(cartData));
 };
 
-const changeAmount = (id, action) => {
-  const item = cartData.find(item => item.id === id);
-
-  if (item) {
-    item.amount = action == '-' ? item.amount - 1 : item.amount + 1;
-    if (item.amount <= 0) {
-      cartData = cartData.filter(item => item.id !== id);
-    }
-    localStorage.setItem('cart', JSON.stringify(cartData));
-  }
-};
-
 loadCart();
 
 cartList.addEventListener('click', e => {
-  const expr = expr => e.classList.contains(expr);
+  const getId = (dom = false) => {
+    let id = e.target;
+    while (id && !id.dataset.id) {
+      id = id.parentElement;
+    }
 
-  if (expr('cart__amount-plus')) {
+    if (!dom) {
+      return id.dataset.id;
+    } else {
+      return id;
+    }
+  };
+  const expr = expr => e.target.classList.contains(expr);
+  const numAct = action => {
+    const id = getId();
+    const item = cartData.find(item => item.id === id);
+
+    if (!item) return;
+
+    if (action === '-' && item.amount <= 1) return;
+
+    item.amount = action === '-' ? item.amount - 1 : item.amount + 1;
+
+    localStorage.setItem('cart', JSON.stringify(cartData));
+
+    const num = getId(true).querySelector('.cart__amount-num');
+    num.textContent =
+      action === '+' ? +num.textContent + 1 : +num.textContent - 1;
+  };
+
+  if (expr('cart__amount-plus')) numAct('+');
+
+  if (expr('cart__amount-minus')) numAct('-');
+
+  if (expr('cart__delete')) {
+    const id = getId();
+    cartData = cartData.filter(item => item.id !== id);
+    localStorage.setItem('cart', JSON.stringify(cartData));
+    loadCart();
   }
+});
+
+deleteAll.addEventListener('click', () => {
+  localStorage.setItem('cart', '');
 });
