9504ac406d791675966a979eface13371909adae
Nazariy <nazarij1608@gmail.com>
2025-04-24 21:42:55 +0300

    added changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 // import { log } from 'handlebars'
 // import template from '../templates/card.hbs'
 
-import { getProducts, getPopularProducts, getDiscproducts } from './products.js'
+import {
+  getProducts,
+  getPopularProducts,
+  getDiscproducts,
+} from './products.js';
 // getProducts().then(pr => console.log('pr', pr))
-getProducts()
-getPopularProducts()
-getDiscproducts()
+getProducts();
+getPopularProducts();
+getDiscproducts();
+
+import './modal.js';
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,40 @@
-//Test
+import { log } from 'handlebars/runtime';
+import modalTemplate from '../templates/modal.hbs';
+
+const modal = document.querySelector('.modal');
+const modalWrapper = document.querySelector('.modal__wrapper');
+const listOfProducts = document.querySelector('.products__items');
+const closeModalBtn = document.querySelector('.modal__close');
+
+const getProductId = async id => {
+  const response = await fetch(
+    `https://food-boutique.b.goit.study/api/products/${id}`
+  );
+  const product = await response.json();
+  return product;
+};
+
+listOfProducts.addEventListener('click', async e => {
+  if (
+    e.target.classList.contains('open-modal-btn') ||
+    e.target.classList.contains('pr__svg') ||
+    e.target.closest('.pr')
+  ) {
+    modal.classList.remove('is-hidden');
+    document.body.style.overflow = 'hidden';
+
+    const id = e.target.closest('.pr').id;
+    console.log('id', id);
+
+    const product = await getProductId(id);
+    console.log('productById', product);
+
+    const modalHTML = modalTemplate(product);
+    modalWrapper.innerHTML = modalHTML;
+  }
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.add('is-hidden');
+  document.body.style.overflow = 'auto';
+});
